c3ba9df230cd06d7230f15e4b6e0df7f8bc742d2
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2017-12-08 19:19:31 +0100

    Added aperture folder

---
+++ aperture/plotAperture.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sttools import STDump, Twiss
+from os import path
+
+def plotAperture(pltBlock=False):
+    
+    
+    return
+
+if __name__ == "__main__":
+    plotAperture(True)
