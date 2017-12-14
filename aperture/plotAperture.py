1bd83be6d8a97022b8287d41dde9e9e43897fccb
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2017-12-14 16:41:41 +0100

    Added aperture plot code

--- aperture/plotAperture.py
+++ aperture/plotAperture.py
@@ -2,14 +2,26 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
-from sttools import STDump, Twiss
-from os import path
+from sttools import TableFS, logger
+from os      import path
 
-def plotAperture(pltBlock=False):
+def plotAperture(aperFile, pltBlock=False):
     
+    if not path.isfile(aperFile):
+        logger.error("File not found %s" % aperFile)
+        return
+    
+    aperData = TableFS(aperFile)
+    aperData.fileInfo()
     
     return
 
 if __name__ == "__main__":
-    plotAperture(True)
+    if len(sys.argv) > 1:
+        plotAperture(sys.argv[1],True)
+    else:
+        logger.error("File name missing")
+    
+# END Main
