b4f2199f743b5043239549eeb1a81d07073ec8fd
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2017-12-15 12:32:10 +0100

    Preliminary aperture plot works

--- aperture/plotAperture.py
+++ aperture/plotAperture.py
@@ -6,6 +6,8 @@ import sys
 
 from sttools import TableFS, logger
 from os      import path
+from array   import array
+from ROOT    import gROOT, TCanvas, TGraph
 
 def plotAperture(aperFile, pltBlock=False):
     
@@ -15,6 +17,53 @@ def plotAperture(aperFile, pltBlock=False):
     
     aperData = TableFS(aperFile)
     aperData.fileInfo()
+    
+    aperS = array("d")
+    aperX = array("d")
+    aperY = array("d")
+    
+    for n in range(aperData.nLines):
+        
+        aperName = aperData.Data["NAME"][n]
+        aperKeyw = aperData.Data["KEYWORD"][n]
+        aperType = aperData.Data["APERTYPE"][n]
+        
+        aperS.append(aperData.Data["S"][n])
+        
+        if   aperType == "RECTELLIPSE":
+            aperX.append(min(aperData.Data["APER_1"][n],aperData.Data["APER_3"][n]))
+            aperY.append(min(aperData.Data["APER_2"][n],aperData.Data["APER_4"][n]))
+        elif aperType == "NONE":
+            aperX.append(0.0)
+            aperY.append(0.0)
+        else:
+            logger.warning("Unhandled APERTYPE '%s'" % aperType)
+            aperX.append(0.0)
+            aperY.append(0.0)
+            
+        # if n >= 100: break
+        
+    for n in range(aperData.nLines):
+        
+        aperName = aperData.Data["NAME"][n]
+        aperKeyw = aperData.Data["KEYWORD"][n]
+        aperType = aperData.Data["APERTYPE"][n]
+        
+        if aperKeyw == "DRIFT" and n < len(aperX) - 1:
+            aperX[n] = aperX[n+1]
+            aperY[n] = aperY[n+1]
+        
+        # print("%16s [%12s] %12s: s=%18.11e x=%18.11e y=%18.11e" % (aperName,aperKeyw,aperType,aperS[n],aperX[n],aperY[n]))
+        # if n >= 100: break
+        
+    gROOT.Reset()
+    cnvMain = TCanvas("c1", "Aperture", 200, 10, 700, 500)
+    tAperX  = TGraph(len(aperS),aperS,aperX)
+    tAperY  = TGraph(len(aperS),aperS,aperY)
+    tAperX.Draw()
+    tAperY.Draw()
+    cnvMain.Update()
+    input("Press Enter to close ...")
     
     return
 
