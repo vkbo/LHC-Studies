7cb85cbee2a2ec2774d5c46f61ed63fb5fc27f47
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2018-01-12 15:03:44 +0100

    Added more apertypes to the plotAperture script

--- aperture/plotAperture.py
+++ aperture/plotAperture.py
@@ -30,16 +30,28 @@ def plotAperture(aperFile, pltBlock=False):
         
         aperS = np.append(aperS,aperData.Data["S"][n])
         
-        if   aperType == "RECTELLIPSE":
+        if   aperType == "CIRCLE":
+            aperX = np.append(aperX,aperData.Data["APER_1"][n])
+            aperY = np.append(aperY,aperData.Data["APER_1"][n])
+        elif aperType == "RECTANGLE":
+            aperX = np.append(aperX,aperData.Data["APER_1"][n])
+            aperY = np.append(aperY,aperData.Data["APER_2"][n])
+        elif aperType == "ELLIPSE":
+            aperX = np.append(aperX,aperData.Data["APER_1"][n])
+            aperY = np.append(aperY,aperData.Data["APER_2"][n])
+        elif aperType == "RECTELLIPSE":
             aperX = np.append(aperX,min(aperData.Data["APER_1"][n],aperData.Data["APER_3"][n]))
             aperY = np.append(aperY,min(aperData.Data["APER_2"][n],aperData.Data["APER_4"][n]))
+        elif aperType == "RACETRACK":
+            aperX = np.append(aperX,aperData.Data["APER_1"][n]+aperData.Data["APER_3"][n])
+            aperY = np.append(aperY,aperData.Data["APER_2"][n]+aperData.Data["APER_4"][n])
         elif aperType == "NONE":
             aperX = np.append(aperX,0.0)
             aperY = np.append(aperY,0.0)
         else:
             logger.warning("Unhandled APERTYPE '%s'" % aperType)
-            aperX = np.append(aperX,0.0)
-            aperY = np.append(aperY,0.0)
+            aperX = np.append(aperX,9.999)
+            aperY = np.append(aperY,9.999)
             
         # if n >= 100: break
         
@@ -56,13 +68,19 @@ def plotAperture(aperFile, pltBlock=False):
         # print("%16s [%12s] %12s: s=%18.11e x=%18.11e y=%18.11e" % (aperName,aperKeyw,aperType,aperS[n],aperX[n],aperY[n]))
         # if n >= 100: break
         
-    aperX = ma.masked_where(aperX > 8.0, aperX)
-    aperY = ma.masked_where(aperY > 8.0, aperY)
+    aperX = ma.masked_where(aperX > 9.9, aperX)
+    aperY = ma.masked_where(aperY > 9.9, aperY)
     
-    plt.step(aperS, aperX, label="X Aperture")
-    plt.step(aperS, aperY, label="Y Aperture")
+    plt.step(aperS, aperX*1e3, label="X Aperture")
+    plt.step(aperS, aperY*1e3, label="Y Aperture")
     
+    plt.xlim(aperS[0], aperS[aperData.nLines-1])
+
     plt.legend()
+    plt.title("Aperture")
+    plt.xlabel("s [m]")
+    plt.ylabel("x, y [mm]")
+    
     plt.show(block=pltBlock)
     
     return
